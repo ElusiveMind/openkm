052827ef95054a0122af21a25d89792b86d1f108
Michael Bagnall <mrbagnall@icloud.com>
2020-06-14 10:58:09 -0400

    KEA demo with OpenKM

--- latest/run.sh
+++ latest/run.sh
@@ -3,11 +3,19 @@ FILE="/opt/tomcat-8.5.34/repository/okmdb.mv.db"
 
 if [ -f "$FILE" ];
 then
-   echo "File $FILE exist. Don't create a DB."
+   echo "File $FILE exist. No setup required."
    sed -i 's/hibernate.hbm2ddl=create/hibernate.hbm2ddl=none/g' /opt/tomcat-8.5.34/OpenKM.cfg
 else
-   echo "File $FILE does not exist. Create a DB."
+   echo "File $FILE does not exist. Begin setup."
 fi
 
-/etc/init.d/openkm start
+cp /root/keas.war /opt/tomcat-8.5.34/webapps/keas.war
+cp /root/vocabulary-sample.zip /opt/tomcat-8.5.34/vocabulary-sample.zip
+cp /root/keas.properties /opt/tomcat-8.5.34/keas.properties
+cd /opt/tomcat-8.5.34
+unzip vocabulary-sample.zip
+
+cd /opt/tomcat-8.5.34/bin
+./startup.sh
+
 tail -f
